89745393d86c6d485afb3b0a23bbcc611d618332
splx <splaandrey@gmail.com>
2022-04-04 22:46:32 +0300

    logging

--- config/logging.php
+++ config/logging.php
@@ -51,14 +51,19 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+//            'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            'level' =>  'debug',
         ],
+//
+//        'single' => [
+//            'driver' => 'single',
+//            'path' => storage_path('logs/laravel.log'),
+//            'level' => env('LOG_LEVEL', 'debug'),
+//        ],
 
         'daily' => [
             'driver' => 'daily',
